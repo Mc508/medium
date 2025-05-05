1c50a0f57f492770fee3287183d3777caa2ae934
Mc508 <mihirc508@gmail.com>
2025-05-05 22:30:30 +0530

    cors added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,19 @@
-import { Hono } from "hono"
-import { decode, sign, verify } from "hono/jwt"
-import { userRouter } from "./routes/user"
-import { blogRouter } from "./routes/blog"
-
+import { Hono } from "hono";
+import { decode, sign, verify } from "hono/jwt";
+import { userRouter } from "./routes/user";
+import { blogRouter } from "./routes/blog";
+import { cors } from "hono/cors";
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string
-    JWT_SECRET: string
-  }
-}>()
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-app.route("/api/v1/user", userRouter)
-app.route("/api/v1/blog", blogRouter)
+app.use("/*",cors())
 
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
-export default app
+export default app;
--- backend/src/zod.ts
+++ backend/src/zod.ts
@@ -1,13 +1,13 @@
 import z from "zod";
 
 export const signupInput = z.object({
-  username: z.string().email(),
-  password: z.string().min(8),
+  email: z.string().email("email must be required"),
+  password: z.string().min(8,"password must be required"),
   name: z.string().optional(),
 });
 
 export const signinInput = z.object({
-  username: z.string().email(),
+  email: z.string().email(),
   password: z.string().min(8),
 });
 
